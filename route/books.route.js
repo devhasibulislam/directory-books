61c57048a1730c713e92d48c84977f9f7ff7a87d
hasibulislam999 <hasib143sl@gmail.com>
2022-09-05 09:29:41 +0600

    books route initialization done

--- route/books.route.js
+++ route/books.route.js
@@ -5,3 +5,26 @@
  * Date: 05/09/2022
  */
 
+// external import
+const express = require("express");
+
+// internal import
+const {
+    getAllBooks: fetchAllBooks,
+    insertABook,
+    fetchABook,
+    updateABook,
+    deleteABook
+} = require("../controller/books.controller");
+
+// router level connection
+const router = express.Router();
+
+// method credentials
+router.get("/", fetchAllBooks);
+router.post("/insert", insertABook);
+router.get("/:id", fetchABook);
+router.put("/:id", updateABook);
+router.delete("/:id", deleteABook)
+
+module.exports = router;
